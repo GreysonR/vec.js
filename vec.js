5aa939a9ad99aff4ae7ddb6539b49b32696eb833
GreysonR <44911101+GreysonR@users.noreply.github.com>
2023-02-04 13:27:19 -0600

    add sign() function

--- vec.js
+++ vec.js
@@ -114,6 +114,14 @@ class vec {
 			return this;
 		}
 	}
+	sign() {
+		return new vec(Math.sign(this.x), Math.sign(this.y));
+	}
+	sign2() {
+		this.x = Math.sign(this.x);
+		this.y = Math.sign(this.y);
+		return this;
+	}
 	mod(vec2) {
 		if (typeof vec2 === "number")
 			return new vec(this.x % vec2, this.y % vec2);
